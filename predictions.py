bcf4f30e46de7052b677286c7d1eb064d96886ee
Alex Dehnert <adehnert@mit.edu>
2013-07-07 17:55:15 -0400

    Add Cambridge-bound MBTA1 to predictions

--- predictions.py
+++ predictions.py
@@ -8,12 +8,16 @@ from lxml.etree import ElementTree, parse
 from StringIO import StringIO
 
 # tech shuttle tracking is broken: tech|wcamp|mass84
-stops = """boston|boston|mass84_d
+saferide_stops = """boston|boston|mass84_d
 northwest|nwcamp|mass77
 saferidebostone|boston|mass84_d
 saferidebostonw|boston|mass84_d
 saferidecambeast|frcamp|mass84_d
 saferidecambwest|frcamp|mass84_d""".split()
+
+mbta_stops = (
+    "1|1_0_var0|97",
+)
 
 def timeandweather():
     # If this ever dies, there's always Microsoft:
@@ -34,15 +38,34 @@ def timeandweather():
         '<img src="%s" /><h1>%s&deg;F</h1>' % (img, cond.get("temp")) + \
         "</td></tr></table>"
 
-url="http://webservices.nextbus.com/service/publicXMLFeed?command=predictionsForMultiStops&a=mit&stops="+"&stops=".join(stops)
-print >>sys.stderr, url
-f = urllib.urlopen(url)
+def minutes(n):
+    n = int(n)
+    if n == 1:
+        return "1 minute"
+    else:
+        return "%s minutes" % n
 
-e = ElementTree(file=f)
+def print_predictions(agency, stops, label=""):
+    url = "http://webservices.nextbus.com/service/publicXMLFeed?command=predictionsForMultiStops&a=" + agency
+    url += "".join(["&stops="+s for s in stops])
+    print >>sys.stderr, url
+    f = urllib.urlopen(url)
 
-predictions = e.findall("//predictions")
-predictions = filter(lambda el: el.find(".//prediction") is not None, predictions)
-predictions.sort(key=lambda el: el.find(".//prediction").get("epochTime"))
+    e = ElementTree(file=f)
+
+    predictions = e.findall("//predictions")
+    predictions = filter(lambda el: el.find(".//prediction") is not None, predictions)
+    predictions.sort(key=lambda el: el.find(".//prediction").get("epochTime"))
+
+    for p in predictions:
+        title = p.get("routeTitle")
+        title = re.sub(r'^Saferide ', '', title)
+        title = label + title
+        print "<h1>"+title+"</h1>"
+        times = p.findall(".//prediction")
+        print '<div class="big">%s</div>' % minutes(times.pop(0).get("minutes"))
+        for t in times[0:2]:
+            print '<div class="small">%s</div>' % minutes(t.get("minutes"))
 
 print "Content-type: text/html"
 print
@@ -57,21 +80,8 @@ body { background: black; color: white }
 <body>"""
 print timeandweather()
 
-def minutes(n):
-    n = int(n)
-    if n == 1:
-        return "1 minute"
-    else:
-        return "%s minutes" % n
-
-for p in predictions:
-    title = p.get("routeTitle")
-    title = re.sub(r'^Saferide ', '', title)
-    print "<h1>"+title+"</h1>"
-    times = p.findall(".//prediction")
-    print '<div class="big">%s</div>' % minutes(times.pop(0).get("minutes"))
-    for t in times[0:2]:
-        print '<div class="small">%s</div>' % minutes(t.get("minutes"))
+print_predictions('mit', saferide_stops)
+print_predictions('mbta', mbta_stops, label="MBTA ")
 
 print """</body>
 </html>"""
